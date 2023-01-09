cd91d1554bbf2d68b501c8900372fcbff36df4a8
abhimanyudhere <abhidhere@1997gmail.com>
2023-01-10 00:21:46 +0530

    added arraylist

---
+++ src/com/git/collection/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.git.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("abhi");
+		list.add("abhi");
+		list.add("abhi");
+		list.add("abhi");
+		System.out.println(list);
+	}
+
+}
