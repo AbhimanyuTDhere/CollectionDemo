546c9e090f98f83382ee81e2dfbbd205a6c5b48f
AbhimanyuTDhere <122228036+AbhimanyuTDhere@users.noreply.github.com>
2023-01-10 01:10:59 +0530

    changredone

--- src/com/git/collection/ArrayListDemo.java
+++ src/com/git/collection/ArrayListDemo.java
@@ -8,7 +8,7 @@ public class ArrayListDemo {
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<String>();
 		list.add("abhi");
-		list.add("abhi");
+		list.add("shubham");
 		list.add("abhi");
 		list.add("abhi");
 		//System.out.println(list);
