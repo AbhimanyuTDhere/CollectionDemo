706ff9c9d69bb6b862dd7fc8b62155fed98c6b5e
abhimanyudhere <abhidhere@1997gmail.com>
2023-01-12 23:41:39 +0530

    s

--- src/com/string/work/MethodsWorking.java
+++ src/com/string/work/MethodsWorking.java
@@ -10,18 +10,11 @@ public class MethodsWorking {
 
 		System.out.println(str1 + "\n" + str2);
 
-<<<<<<< HEAD
 		String str11 = "Abhimanyu tuara Dhere";
 		String str21 = new String("Abhi");
 		String str12 = str1.toLowerCase();
 		String str3 = str21.toUpperCase();
-
 		System.out.println(str1 + "\n " + str3);
-=======
-            String str3 = str21.toUpperCase();
-	    //System.out.println(str1.equal(str11);
-	    System.out.println(str1 +"\n "+str3);
->>>>>>> fd1f200c7735719da2038f7df7ae014e0c4a5e2f
 
 	}
 }
