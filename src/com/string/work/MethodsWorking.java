dc7eca12ff9b2f1bdea1412ed067a8176a587c1b
AbhimanyuTDhere <122228036+AbhimanyuTDhere@users.noreply.github.com>
2023-01-12 22:59:38 +0530

    Update MethodsWorking.java
    final changes check

--- src/com/string/work/MethodsWorking.java
+++ src/com/string/work/MethodsWorking.java
@@ -3,8 +3,10 @@ package com.string.work;
 public class MethodsWorking {
 
 	public static void main(String[] args) {
-		String str  ="Abhimanyu Dhere";
-	    String str1 = str.toLowerCase();
+		String str1  ="Abhimanyu Dhere";
+		String str2= new String("Abhi");
+	    String str = str1.toLowerCase();
+	    System.out.println(str1.equal(str2);
 	    System.out.println(str1);
 	}
 }
