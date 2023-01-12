d41ae0101d40e2e70bc10c8324f2eaace6265abb
abhimanyudhere <abhidhere@1997gmail.com>
2023-01-12 23:13:11 +0530

    change method

--- src/com/string/work/MethodsWorking.java
+++ src/com/string/work/MethodsWorking.java
@@ -3,8 +3,10 @@ package com.string.work;
 public class MethodsWorking {
 
 	public static void main(String[] args) {
-		String str  ="Abhimanyu Dhere";
-	    String str1 = str.toLowerCase();
-	    System.out.println(str1);
+		String str = "Abhimanyu Dhere";
+		String str1 = str.toLowerCase();
+		char str2 = str.charAt(5);
+
+		System.out.println(str1 +"\n"+ str2);
 	}
 }
