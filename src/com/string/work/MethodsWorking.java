5ba4990c45928aff708764c8bb9c4af1ecb05e56
abhimanyudhere <abhidhere@1997gmail.com>
2023-01-12 22:53:52 +0530

    chnges done

---
+++ src/com/string/work/MethodsWorking.java
@@ -0,0 +1,10 @@
+package com.string.work;
+
+public class MethodsWorking {
+
+	public static void main(String[] args) {
+		String str  ="Abhimanyu Dhere";
+	    String str1 = str.toLowerCase();
+	    System.out.println(str1);
+	}
+}
